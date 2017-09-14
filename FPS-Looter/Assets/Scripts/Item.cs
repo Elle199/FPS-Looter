95625bfdf94616b1ce44a19ccc03d4355467a1df
DESKTOP-MONKEYL\Meawsome <elliot.angman@gmail.com>
2017-09-14 23:02:58 +0200

    Added project and setup initial UI and Movement

---
+++ FPS-Looter/Assets/Scripts/Item.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Item")]
+public class Item : ScriptableObject
+{
+    public string item_name = "New Item";
+    public Sprite icon = null;
+    public bool isDefaultItem = false;
+}
